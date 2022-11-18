f3708dfb6259a1aaeee6970db2d4c0c2f5d4b9b4
Emanuel Farcau <emanuelfarcau@gmail.com>
2022-11-18 10:34:19 +0200

    Initial Commit

---
+++ EventHub/EventHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventHubApp.swift
+//  EventHub
+//
+//  Created by internship on 18.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct EventHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
